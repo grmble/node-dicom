2acae6880f214896e9f2ef234d0ed7657f311daf
Juergen Gmeiner <spamless.juergen@gmail.com>
2012-03-28 15:48:49 +0200

    first steps of parsebuffer based decoder

---
+++ test/test-parsebuffer.js
@@ -0,0 +1,50 @@
+var parsebuffer = require('../lib/parsebuffer');
+
+var testCounter = 0;
+function testBuffer (length) {
+    var buff = new Buffer(length);
+    for(var i = 0; i < length; i++) {
+        buff[i] = testCounter++;
+    }
+    return buff;
+}
+
+exports.testRequest = function (test) {
+    test.expect(1);
+
+    var pb = new parsebuffer.ParseBuffer();
+    var result = [];
+    pb.request(8, parsebuffer.setter(result));
+    pb.request(8, parsebuffer.setter(result));
+    pb.request(4, parsebuffer.setter(result, function () {
+        test.deepEqual(result, [new Buffer([0,1,2,3,4,5,6,7]),
+            new Buffer([8,9,10,11,12,13,14,15]),
+            new Buffer([16,17,18,19])]);
+    }));
+
+    pb.onData(testBuffer(6));
+    pb.onData(testBuffer(6));
+    pb.onData(testBuffer(4));
+    pb.onData(testBuffer(4));
+
+    test.done();
+};
+
+exports.testGroup = function (test) {
+    test.expect(1);
+
+    var pb = new parsebuffer.ParseBuffer();
+    var result = [];
+    pb.enterGroup(20, function() {
+        test.deepEqual(result, [new Buffer([20,21,22,23,24,25,26,27]),
+            new Buffer([28,29,30,31,32,33,34,35]),
+            new Buffer([36,37,38,39])]);
+    });
+    pb.request(8, parsebuffer.setter(result));
+    pb.request(8, parsebuffer.setter(result));
+    pb.request(4, parsebuffer.setter(result));
+
+    pb.onData(testBuffer(20));
+
+    test.done();
+};
--- test/test-vr.js
+++ test/test-vr.js
@@ -3,11 +3,11 @@ var VR = require('../lib/vr');
 exports.testUL = function(test) {
     test.expect(4);
 
-    var ul = VR.LE.UL;
+    var ul = new VR.LE.UL();
     test.deepEqual(ul.decode(new Buffer([1,2,3,4])), [0x04030201]);
     test.deepEqual(ul.decode(new Buffer([1,2,3,4,5,6,7,8])), [0x04030201, 0x08070605]);
 
-    ul = VR.BE.UL;
+    ul = new VR.BE.UL();
     test.deepEqual(ul.decode(new Buffer([1,2,3,4])), [0x01020304]);
     test.deepEqual(ul.decode(new Buffer([1,2,3,4,5,6,7,8])), [0x01020304, 0x05060708]);
 
@@ -17,11 +17,11 @@ exports.testUL = function(test) {
 
 exports.testValueLengthBytes = function(test) {
     test.expect(4);
-    var ul = VR.LE.UL;
+    var ul = new VR.LE.UL();
     test.equal(ul.valueLengthBytes(false), 2);
     test.equal(ul.valueLengthBytes(true), 4);
 
-    var ob = VR.LE.OB;
+    var ob = new VR.LE.OB();
     test.equal(ob.valueLengthBytes(false), 6)
     test.equal(ob.valueLengthBytes(true), 4)
 
@@ -31,7 +31,7 @@ exports.testValueLengthBytes = function(test) {
 exports.testUI = function(test) {
     test.expect(1);
 
-    var ui = VR.LE.UI;
+    var ui = new VR.LE.UI();
 
     test.deepEqual(ui.decode(new Buffer("1.2.3.4")), ["1.2.3.4"]);
     test.done();
@@ -41,7 +41,7 @@ exports.testOB = function(test) {
     test.expect(2);
 
     var input = new Buffer([1,2,3,4,5,6,7,8]),
-        output = VR.LE.OB.decode(input);
+        output = (new VR.LE.OB()).decode(input);
 
     test.equal(1, output.length);
     test.equal(input.toString('binary'), output.toString('binary'));
@@ -57,8 +57,8 @@ exports.testOF = function(test) {
     bePi.writeFloatBE(3.14, 0);
     lePi.writeFloatLE(3.14, 0);
 
-    var beResult = VR.BE.OF.decode(bePi);
-    var leResult = VR.LE.OF.decode(lePi);
+    var beResult = (new VR.BE.OF()).decode(bePi);
+    var leResult = (new VR.LE.OF()).decode(lePi);
 
     test.equal(1, beResult.length);
     test.equal(1, leResult.length);
@@ -72,7 +72,7 @@ exports.testPN = function(test) {
     test.expect(1);
 
     var pnBuff = new Buffer("Spamless^Juergen\\Grmble");
-    test.deepEqual(VR.LE.PN.decode(pnBuff), ["Spamless^Juergen", "Grmble"]);
+    test.deepEqual((new VR.LE.PN()).decode(pnBuff), ["Spamless^Juergen", "Grmble"]);
 
     test.done();
 };
@@ -81,7 +81,7 @@ exports.testST = function(test) {
     test.expect(1);
 
     var stBuff = new Buffer("Spamless^Juergen\\Grmble");
-    test.deepEqual(VR.LE.ST.decode(stBuff), ["Spamless^Juergen\\Grmble"]);
+    test.deepEqual((new VR.LE.ST()).decode(stBuff), ["Spamless^Juergen\\Grmble"]);
 
     test.done();
 };
