3232d042c463a8518fe415ebb99a1978ed9a5ceb
Juergen Gmeiner <spamless.juergen@gmail.com>
2012-03-28 17:52:28 +0200

    WIP: implicit group loops, still not quite right

--- test/test-parsebuffer.js
+++ test/test-parsebuffer.js
@@ -31,18 +31,26 @@ exports.testRequest = function (test) {
 };
 
 exports.testGroup = function (test) {
-    test.expect(1);
+    test.expect(3);
 
     var pb = new parsebuffer.ParseBuffer();
     var result = [];
-    pb.enterGroup(20, function() {
+    var theGroup = pb.enterGroup(20, function() {
+        console.log("XXXXXXXXXXX");
         test.deepEqual(result, [new Buffer([20,21,22,23,24,25,26,27]),
             new Buffer([28,29,30,31,32,33,34,35]),
             new Buffer([36,37,38,39])]);
+        test.ok(! theGroup.active);
     });
     pb.request(8, parsebuffer.setter(result));
     pb.request(8, parsebuffer.setter(result));
-    pb.request(4, parsebuffer.setter(result));
+    pb.request(4, function (buff) {
+        result.push(buff);
+        console.debug("final callback", buff);
+        // in the callback of the last group member, this is false
+        // before reading the dataelement value, it would be true
+        test.ok(! theGroup.active);
+    });
 
     pb.onData(testBuffer(20));
 
